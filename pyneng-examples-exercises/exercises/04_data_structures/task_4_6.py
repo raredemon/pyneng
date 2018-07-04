92053abfe63f1711c744f0fb87085acbfb0cafde
raredemon <raredemon@gmail.com>
2018-07-04 15:57:56 +0300

    exercise 4.6

--- pyneng-examples-exercises/exercises/04_data_structures/task_4_6.py
+++ pyneng-examples-exercises/exercises/04_data_structures/task_4_6.py
@@ -6,7 +6,7 @@
 Protocol:              OSPF
 Prefix:                10.0.24.0/24
 AD/Metric:             110/41
-Next-Hop:              10.0.13.3
+Next-Hop:              10.0.13.3[<32;89;11M
 Last update:           3d18h
 Outbound Interface:    FastEthernet0/0
 
@@ -15,8 +15,16 @@ Outbound Interface:    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
-
 value = ospf_route.split()
 value.remove('O')
 value.insert(0,'OSPF')
-val = str(value)
+ip_template = '''
+Protocol:           {}
+Prefix:             {}
+AD/Metric:          {}
+Next-Hop:           {}
+Last update:        {}
+Outbound Interface: {}
+'''
+proto, prefix, metric, via, gw, uptime, interface = value
+print(ip_template.format(proto, prefix, metric.strip('[]'), gw.strip(','), uptime.strip(','), interface))
